aef18d3f298fbb29beff73bf42be3c05c3c8dc84
Psverma0807 <psverma01979@gmail.com>
2026-04-14 10:50:40 +0530

    fix socket connection

--- app.py
+++ app.py
@@ -97,4 +97,7 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host="0.0.0.0", port=port)
-    
+
+@socketio.on('join_game')
+def join_game(data):
+    print("JOIN EVENT:", data)  
